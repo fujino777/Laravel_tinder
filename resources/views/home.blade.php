0d034d13eed384e6167cce8dd91299ce252fe19b
fujino <87427619+fujino777@users.noreply.github.com>
2022-04-15 15:41:32 +0900

    7-2 home ビューファイル編集

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,16 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
  
 @extends('layouts.layout')
+
 @section('content')
+
 <div class="topPage">
  <nav class="nav">
  <ul>
  <li class="personIcon">
- <a href="/users/show/{{Auth::id()}}"><i class="fas fa-user fa-2x">
-</i></a></li>
- <li class="appIcon"><a href="{{route('home')}}"><img
-src="/storage/images/techpit-match-icon.png"></a></li>
+ <a href="/users/show/{{Auth::id()}}"><i class="fas fa-user fa-2x"> </i></a></li>
+ <li class="appIcon"><a href="{{route('home')}}"><img src="/storage/images/techpit-match-icon.png"></a></li>
+ <li class="messageIcon"><a href="{{route('matching')}}"><i class="fas fa-2x fa-comments"></a></i></li>
  </ul>
  </nav>
  <div id="tinderslide">
@@ -18,7 +19,6 @@ src="/storage/images/techpit-match-icon.png"></a></li>
  <li data-user_id="{{ $user->id }}">
  <div class="userName">{{ $user->name }}</div>
  <img src="/storage/images/{{$user->img_name}}">
- <!-- {{var_dump($user->img_name)}} -->
  <div class="like"></div>
  <div class="dislike"></div>
  </li>
@@ -31,4 +31,9 @@ src="/storage/images/techpit-match-icon.png"></a></li>
  <a href="#" class="like"><i class="fas fa-heart fa-2x"></i></a>
  </div>
 </div>
+
+<script>
+ var usersNum = {{ $userCount }};
+ var from_user_id = {{ $from_user_id }};
+</script>
 @endsection
