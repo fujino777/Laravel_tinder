269a37e5dd551d0455846e8c19d7f6d50c9a2ddb
fujino <87427619+fujino777@users.noreply.github.com>
2022-04-19 22:14:47 +0900

    8-1 作成したモデルにリレーションを追記

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,25 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function toUserId()
+    {
+        return $this->hasMany('App\Reaction', 'to_user_id', 'id');
+    }
+
+    public function fromUserId()
+    {
+        return $this->hasMany('App\Reaction', 'from_user_id', 'id');
+    }
+
+    public function chatMessages()
+    {
+        return $this->hasMany('App\ChatMessage');
+    }
+
+    public function chatRoomUsers()
+    {
+        return $this->hasMany('App\ChatRoomUsers');
+    }
+
 }
